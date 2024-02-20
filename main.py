47ad9a8cbd403ef62ce42b89fb7ece31e860a444
boehmax <maximilian.bohm@kemi.uu.se>
2024-02-20 21:07:01 +0100

    Added a dummy main.py file, will moste likely be replaced and moddified later.

---
+++ main.py
@@ -0,0 +1,10 @@
+from protein_to_genome import ProteinToGenome 
+
+
+def main():
+    print("Thank you for using protein-to-genome!")
+    print("created February 2024")
+    ProteinToGenome.run()
+
+if __name__ == "__main__"
+    main()
